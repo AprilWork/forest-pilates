6fb1a06394555a8182fcdd1e2fc3fdacb981b881
Irene Mitin <IraDelphi@gmail.com>
2022-08-14 18:45:58 -0400

    Added test method order in class GroupsDAOTest

--- forest-001/src/test/java/com/kukvar/hibernate/DAO/GroupsDAOTest.java
+++ forest-001/src/test/java/com/kukvar/hibernate/DAO/GroupsDAOTest.java
@@ -1,27 +1,25 @@
 package com.kukvar.hibernate.DAO;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import java.util.List;
-
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.params.ParameterizedTest;
-//import org.junit.jupiter.params.provider.NullAndEmptySource;
-//import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import com.kukvar.hibernate.entity.Group;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class GroupsDAOTest {
-static final String NAME_1 = "Basic Yoga";
-static final String NAME_2 = "Basic Yoga with image";
+static final String NAME = "Tested name";
 static final String DESCRIPTION = "Wonderfull yoga class for beginners. All age invited!";
 static final String IMAGE = "image.jpg";
-int idGroup1, id_delete; 
-Group testedGroup1, testedGroupDelete;
+static int id; 
+private Group testedGroup = new Group(NAME, DESCRIPTION, IMAGE);
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
@@ -33,78 +31,67 @@ Group testedGroup1, testedGroupDelete;
 
 	@BeforeEach
 	void setUp() throws Exception {
-		idGroup1 = 20;
-		id_delete = 100;		
-		testedGroup1 = new Group(idGroup1,NAME_1, DESCRIPTION);
-		testedGroupDelete = new Group(id_delete,NAME_1, DESCRIPTION, null);
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
-		try {
-			new GroupsDAO().deleteGroup(idGroup1);
-			new GroupsDAO().deleteGroup(id_delete);
-		} catch (Exception e) {
-		}
 	}
 
-
 	@Test
+	@Order(1)
+	final void testAddGroupDetails() {
+		id = new GroupsDAO().addGroupDetails(testedGroup);
+		assertNotNull(new GroupsDAO().getGroup(id), "The category not added.");
+	}
+	
+	@Test
+	@Order(2)
 	final void testGetGroupViaName() {
-		new GroupsDAO().addGroupDetails(testedGroup1);
-		assertNotNull(new GroupsDAO().getGroup(NAME_1), "Do not return class Group.");
-		assertEquals(NAME_1, new GroupsDAO().getGroup(NAME_1).getName(),"Return wrong Group");		
+		assertNotNull(new GroupsDAO().getGroup(NAME), "Do not return class Group.");
+		assertEquals(NAME, new GroupsDAO().getGroup(NAME).getName(),"Return wrong Group");		
 	}	
 	
 	@Test
-	final void testAddGroupDetails() {
-		new GroupsDAO().addGroupDetails(testedGroup1);
-		assertNotNull(new GroupsDAO().getGroup(idGroup1), "The category not added.");
-	}
-
-	@Test
+	@Order(3)
 	final void testListGroups() {
 		List<Group> groups = new GroupsDAO().listGroups();
 		assertNotNull(groups, "The groups list do not returned.");		
 	}
 
-
 	@Test
-	final void testUpdateInformation() {
-		new GroupsDAO().addGroupDetails(testedGroup1);
-		
-		new GroupsDAO().updateInformation(idGroup1, NAME_1, "updated"+DESCRIPTION, IMAGE);
-		assertEquals("updated"+DESCRIPTION, new GroupsDAO().getGroup(idGroup1).getDescription(),"The group's description do not updated.");
-		
-		new GroupsDAO().updateInformation(idGroup1, NAME_2, DESCRIPTION, IMAGE);
-		assertEquals(NAME_2, new GroupsDAO().getGroup(idGroup1).getName(),"The group's name do not updated.");
-		
-		new GroupsDAO().updateInformation(idGroup1, NAME_1, DESCRIPTION, "updated"+IMAGE);
-		assertEquals("updated"+IMAGE, new GroupsDAO().getGroup(idGroup1).getNameImageFile(),"The group's image do not updated.");
-		
-		new GroupsDAO().updateInformation(idGroup1, NAME_1, DESCRIPTION, null);
-		assertEquals(null, new GroupsDAO().getGroup(idGroup1).getNameImageFile(),"The group's image do not updated.");	
-	}
-
-	@Test
-	final void testGetGroup() {
-		new GroupsDAO().addGroupDetails(testedGroup1);
-		assertNotNull(new GroupsDAO().getGroup(idGroup1), "Do not return class Group.");
-		assertEquals(idGroup1, new GroupsDAO().getGroup(idGroup1).getId(),"Return wrong Group id");
-	}
-
-	@Test
+	@Order(4)
 	final void testIsExisted() {
-		new GroupsDAO().addGroupDetails(testedGroup1);
-		assertTrue(new GroupsDAO().isExisted(NAME_1),"A user existed but returned not existed result.");
+		assertTrue(new GroupsDAO().isExisted(NAME),"A user existed but returned not existed result.");
+	}
+	
+	@Test
+		@Order(5)
+	final void testGetGroup() {
+		assertNotNull(new GroupsDAO().getGroup(id), "Do not return class Group.");
+		assertEquals(id, new GroupsDAO().getGroup(id).getId(),"Return wrong Group id");
+	}	
+	
+	@Test
+	@Order(6)
+	final void testUpdateInformation() {
+		new GroupsDAO().updateInformation(id, NAME, DESCRIPTION+1, IMAGE);
+		assertEquals(DESCRIPTION+1, new GroupsDAO().getGroup(id).getDescription(),"The group's description do not updated.");
+		
+		new GroupsDAO().updateInformation(id, NAME+1, DESCRIPTION, IMAGE);
+		assertEquals(NAME+1, new GroupsDAO().getGroup(id).getName(),"The group's name do not updated.");
+		
+		new GroupsDAO().updateInformation(id, NAME, DESCRIPTION, "updated"+IMAGE);
+		assertEquals("updated"+IMAGE, new GroupsDAO().getGroup(id).getNameImageFile(),"The group's image do not updated.");
+		
+		new GroupsDAO().updateInformation(id, NAME, DESCRIPTION, null);
+		assertEquals(null, new GroupsDAO().getGroup(id).getNameImageFile(),"The group's image do not updated.");	
 	}
 
 	@Test
+	@Order(7)
 	final void testDeleteGroup() {
-		new GroupsDAO().addGroupDetails(testedGroupDelete);
-		new GroupsDAO().deleteGroup(id_delete);
-		testedGroupDelete = new GroupsDAO().getGroup(id_delete);
-		assertTrue(testedGroupDelete == null, "The group do not deleted.");			
+		new GroupsDAO().deleteGroup(id);
+		assertNull(new GroupsDAO().getGroup(id), "The group do not deleted.");			
 	}
 
 }
