df66dbccb8489ea06698c0f81d3bd8641ce7c1e7
Irene Mitin <IraDelphi@gmail.com>
2022-08-14 10:19:37 -0400

    Added Annotated Classes to method buildSessionFactory

--- forest-001/src/main/java/com/kukvar/utils/HibernateUtil.java
+++ forest-001/src/main/java/com/kukvar/utils/HibernateUtil.java
@@ -5,19 +5,25 @@ import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+import com.kukvar.hibernate.entity.Category;
+import com.kukvar.hibernate.entity.Customers;
+import com.kukvar.hibernate.entity.Group;
 
 public class HibernateUtil {
 	
     private static final SessionFactory sessionFactory = buildSessionFactory();
     
     private static SessionFactory buildSessionFactory() {
-        try {        	
-            // Create the SessionFactory from hibernate.cfg.xml
-            
-            StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
-            Metadata metadata = new MetadataSources(serviceRegistry).getMetadataBuilder().build();
-            return metadata.getSessionFactoryBuilder().build();
-            
+        try {    
+        	 // Create the SessionFactory from hibernate.cfg.xml
+        	Configuration configuration = new Configuration()
+        			.configure("hibernate.cfg.xml")
+        			.addAnnotatedClass(Group.class)
+        			.addAnnotatedClass(Category.class)
+        			.addAnnotatedClass(Customers.class);
+          return configuration.buildSessionFactory();  
         }
         catch (Throwable ex) {
             // Make sure you log the exception, as it might be swallowed
