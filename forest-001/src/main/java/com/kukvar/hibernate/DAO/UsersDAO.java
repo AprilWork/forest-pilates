1c6d17207a99974df4a4b363468f1d85ea9748c3
Irene Mitin <IraDelphi@gmail.com>
2022-08-14 14:07:10 -0400

    FilesDAO: initialization of variable factory, added closing  session

--- forest-001/src/main/java/com/kukvar/hibernate/DAO/UsersDAO.java
+++ forest-001/src/main/java/com/kukvar/hibernate/DAO/UsersDAO.java
@@ -4,51 +4,51 @@ import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-
 import com.kukvar.hibernate.entity.Customers;
+import com.kukvar.utils.HibernateUtil;
 
 public class UsersDAO {
-	SessionFactory factory = new Configuration()
-			.configure("hibernate.cfg.xml")
-			.addAnnotatedClass(Customers.class)
-			.buildSessionFactory();
+	SessionFactory factory = HibernateUtil.getSessionFactory();
 	
 	public void addCustomersDetails(Customers customer) {
-		Session session = factory.getCurrentSession();
+		Session session = factory.openSession();
 		session.beginTransaction();
 		session.save(customer);
 		session.getTransaction().commit();
+		session.close();
 	}	
 	
 	public List<Customers> listCustomers() {
-		Session session = factory.getCurrentSession();
+		Session session = factory.openSession();
 		session.beginTransaction();
 		@SuppressWarnings("unchecked")
 		List<Customers> customers = session.createQuery("from users").getResultList();
-		//session.getTransaction().commit();
+		session.getTransaction().commit();
+		session.close();
 		return customers;
 	}	
 	
 	public void updateInformation(int id, String email, String username) {
-		Session session = factory.getCurrentSession();
+		Session session = factory.openSession();
 		session.beginTransaction();
 		Customers customer = session.get(Customers.class, id);
 		customer.setEmail(email);
 		customer.setUsername(username);
 		session.getTransaction().commit();
+		session.close();
 	}	
 	
 	public Customers getCustomer(int id) {
-		Session session = factory.getCurrentSession();
+		Session session = factory.openSession();
 		session.beginTransaction();
 		Customers customer = session.get(Customers.class, id);
-		session.getTransaction().commit();		
+		session.getTransaction().commit();
+		session.close();
 		return customer;
 	}	
 	
 	public Customers getCustomer(String email) {
-		Session session = factory.getCurrentSession();
+		Session session = factory.openSession();
 		session.beginTransaction();
 		String queryString = "from users where email = '"+email+"'";
 		Customers customer;
@@ -58,15 +58,17 @@ public class UsersDAO {
 			// TODO: handle exception
 			return null;
 		}
-		session.getTransaction().commit();		
+		session.getTransaction().commit();
+		session.close();
 		return customer;
 	}
 	
 	public boolean isExisted(String email) {
-		Session session = factory.getCurrentSession();
+		Session session = factory.openSession();
 		session.beginTransaction();
 		String queryString = "from users where email = '"+email+"'";
 		int size =  session.createQuery(queryString).getResultList().size();
+		session.close();
 		if (size == 0) {
 			return false;
 		} else {
@@ -75,13 +77,14 @@ public class UsersDAO {
 	}	
 	
 	public void deleteCustomer(int id) {
-		Session session = factory.getCurrentSession();
+		Session session = factory.openSession();
 		session.beginTransaction();		
 		Customers customer = session.get(Customers.class, id);
 		if (customer != null) {
 			session.delete(customer);
-			session.getTransaction().commit();
 		}
+		session.getTransaction().commit();
+		session.close();
 	}
 	
 }
