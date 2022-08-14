23a578519d842a9d79cfbd59983c42553c68130e
Irene Mitin <IraDelphi@gmail.com>
2022-08-14 17:55:44 -0400

    Added test method order in CategoryDAOTest

--- forest-001/src/test/java/com/kukvar/hibernate/DAO/CategoryDAOTest.java
+++ forest-001/src/test/java/com/kukvar/hibernate/DAO/CategoryDAOTest.java
@@ -7,16 +7,18 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import com.kukvar.hibernate.entity.Category;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class CategoryDAOTest {
-	private static final String TESTED_CATEGORY_NAME = "Tested";
-	private static final String TESTED_CATEGORY_DELETE = "Tested_delete";
-	private Category testedCategoryDelete;
-	private Category testedCategory;
-	private int id, id_delete;
+	private static final String NAME = "Tested";
+	private Category testedCategory = new Category(NAME);
+	static int id;
 	
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
@@ -28,67 +30,58 @@ class CategoryDAOTest {
 
 	@BeforeEach
 	void setUp() throws Exception {
-		id = 20;
-		id_delete = 120;
-		testedCategory = new Category(id,TESTED_CATEGORY_NAME);
-		testedCategoryDelete = new Category(id_delete,TESTED_CATEGORY_DELETE);
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
-		
-		try {
-			new CategoryDAO().deleteCategory(id);
-			new CategoryDAO().deleteCategory(id_delete);
-		} catch (Exception e) {
-			e.getStackTrace();
-		}
 	}
 
 	@Test
-	final void testAddCategoryDetails() {
-		new CategoryDAO().addCategoryDetails(testedCategory);
-		assertNotNull(new CategoryDAO().getCategory(id), "The category not added.");
-	}
-
-	@Test
+	@Order(1)
 	final void testListCategory() {
 		List<Category> categories = new CategoryDAO().listCategory();
 		assertNotNull(categories, "The categories list do not returned.");
 	}
-
+	
 	@Test
-	final void testUpdateInformation() {
-		new CategoryDAO().addCategoryDetails(testedCategory);
-		new CategoryDAO().updateInformation(id, TESTED_CATEGORY_DELETE);
-		assertEquals(TESTED_CATEGORY_DELETE, new CategoryDAO().getCategory(id).getName(),"The category's name do not updated.");
+	@Order(2)
+	final void testAddCategoryDetails() {
+		id = new CategoryDAO().addCategoryDetails(testedCategory);
+		assertNotNull(new CategoryDAO().getCategory(id), "The category not added.");
 	}
-
+	
 	@Test
-	final void testGetCategoryInt() {
-		new CategoryDAO().addCategoryDetails(testedCategory);
-		assertNotNull(new CategoryDAO().getCategory(id), "Do not return class Category.");
-		assertEquals(TESTED_CATEGORY_NAME,new CategoryDAO().getCategory(id).getName(), "Return wrong category via name");
-	}
-
-	@Test
-	final void testGetCategoryString() {
-		new CategoryDAO().addCategoryDetails(testedCategory);
-		assertNotNull(new CategoryDAO().getCategory(TESTED_CATEGORY_NAME), "Do not return class Category.");
-		assertEquals(TESTED_CATEGORY_NAME,new CategoryDAO().getCategory(TESTED_CATEGORY_NAME).getName(), "Return wrong category via name");
-	}
-
-	@Test
+	@Order(3)
 	final void testIsExisted() {
-		new CategoryDAO().addCategoryDetails(testedCategory);
-		assertTrue(new CategoryDAO().isExisted(TESTED_CATEGORY_NAME), "Category existed  but return false");
+		assertTrue(new CategoryDAO().isExisted(NAME), "Category existed  but return false");
+	}	
+	
+	@Test
+	@Order(4)
+	final void testGetCategoryString() {
+		assertNotNull(new CategoryDAO().getCategory(NAME), "Do not return class Category.");
+		assertEquals(NAME,new CategoryDAO().getCategory(NAME).getName(), "Return wrong category via name");
+	}	
+
+	@Test
+	@Order(5)
+	final void testGetCategoryInt() {
+		assertNotNull(new CategoryDAO().getCategory(id), "Do not return class Category.");
+		assertEquals(NAME,new CategoryDAO().getCategory(id).getName(), "Return wrong category via name");
 	}
 
 	@Test
+	@Order(6)
+	final void testUpdateInformation() {
+		new CategoryDAO().updateInformation(id, NAME+1);
+		assertEquals(NAME+1, new CategoryDAO().getCategory(id).getName(),"The category's name do not updated.");
+	}	
+
+	@Test
+	@Order(7)
 	final void testDeleteCategory() {
-		new CategoryDAO().addCategoryDetails(testedCategoryDelete);
-		new CategoryDAO().deleteCategory(id_delete);
-		assertNull(new CategoryDAO().getCategory(id_delete), "The class category do not deleted.");		
+		new CategoryDAO().deleteCategory(id);
+		assertNull(new CategoryDAO().getCategory(id), "The class category do not deleted.");		
 	}
 
 }
