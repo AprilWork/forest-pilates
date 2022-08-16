fa4ab7917e1e0640c19ade3c1607e08f4297ad38
Irene Mitin <IraDelphi@gmail.com>
2022-08-16 16:24:37 -0400

    Group: check null, value category,relationship, changed toString

--- forest-001/src/main/java/com/kukvar/hibernate/entity/Group.java
+++ forest-001/src/main/java/com/kukvar/hibernate/entity/Group.java
@@ -5,6 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity(name="classes")
@@ -16,20 +18,24 @@ public class Group {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="id")
 	private int id;
-	@Column(name="name")
+	@Column(name="name", nullable=false)
 	private String name;	
 	@Column(name="description")
 	private String description;	
 	@Column(name="file_image_name")
 	private String nameImageFile;
+	@ManyToOne
+	@JoinColumn(name="type_id")
+	private Category category;
 	
 	protected Group() {	}
 
-	public Group(String name, String description, String nameImageFile) {
+	public Group(String name, String description, String nameImageFile, Category category) {
 		super();
 		this.name = name;
 		this.description = description;
 		this.nameImageFile = imageNameHelper(nameImageFile);
+		this.category = category;
 	}
 	
 	public Group(String name, String description) {
@@ -75,11 +81,19 @@ public class Group {
 	public void setNameImageFile(String nameImageFile) {
 		this.nameImageFile = nameImageFile;
 	}
+	
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
 
 	@Override
 	public String toString() {
 		return "Group [id=" + id + ", name=" + name + ", description=" + description + ", nameImageFile=" + nameImageFile
-				+ "]";
+				+ ", category=" + category.getName() + "]";
 	}
 	
 }
