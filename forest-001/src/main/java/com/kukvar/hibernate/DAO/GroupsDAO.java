eca7316224554ecd617b7cf40b2387b7dc83fb9a
Irene Mitin <IraDelphi@gmail.com>
2022-08-31 10:45:19 -0400

    GroupsDAO methods with try/catch/final blocks

--- forest-001/src/main/java/com/kukvar/hibernate/DAO/GroupsDAO.java
+++ forest-001/src/main/java/com/kukvar/hibernate/DAO/GroupsDAO.java
@@ -1,5 +1,6 @@
 package com.kukvar.hibernate.DAO;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.hibernate.Session;
@@ -14,77 +15,136 @@ import com.kukvar.hibernate.utils.HibernateUtil;
 public class GroupsDAO {
 	SessionFactory factory = HibernateUtil.getSessionFactory();
 
-	public int addGroupDetails(Group group) {
+	public int addGroupDetails(Group group) {		
 		Session session = factory.openSession();
-		session.beginTransaction();
-		int id = (int) session.save(group);
-		session.getTransaction().commit();
-		session.close();
-		return id;
+		Transaction txn = session.getTransaction();
+		int id = 0;
+		try {
+			txn.begin();
+			id = (int) session.save(group);
+			txn.commit();
+		} catch (Exception e) {
+			if (txn != null) {
+				e.printStackTrace();
+				txn.rollback();
+			}
+		} finally {
+			if (session != null) {
+				session.close();
+			}
+		}	
+		return id;		
 	}
 
+	@SuppressWarnings("unchecked")
 	public List<Group> listGroups() {
 		Session session = factory.openSession();
-		session.beginTransaction();
-		@SuppressWarnings("unchecked")
-		List<Group> groups = session.createQuery("from classes").getResultList();
-		session.getTransaction().commit();
-		session.close();
+		Transaction txn = session.getTransaction();
+		List<Group> groups = new ArrayList<>();
+		try {
+			txn.begin();
+			groups = session.createQuery("from classes").getResultList();
+			txn.commit();
+		} catch (Exception e) {
+			if (txn != null) {
+				e.printStackTrace();
+				txn.rollback();
+			}
+		} finally {
+			if (session != null) {
+				session.close();
+			}
+		}	
 		return groups;
 	}
 
 	public void updateInformation(int id, String name, String description, String nameImageFile, Category category) {
 		Session session = factory.openSession();
-		session.beginTransaction();
-		Group group = session.get(Group.class, id);
-		group.setId(id);
-		group.setName(name);
-		group.setDescription(description);
-		group.setNameImageFile(nameImageFile);
-		group.setCategory(category);
-		session.getTransaction().commit();
-		session.close();
+		Transaction txn = session.getTransaction();
+		try {
+			txn.begin();
+			Group group = session.get(Group.class, id);
+			group.setId(id);
+			group.setName(name);
+			group.setDescription(description);
+			group.setNameImageFile(nameImageFile);
+			group.setCategory(category);			
+			txn.commit();
+		} catch (Exception e) {
+			if (txn != null) {
+				e.printStackTrace();
+				txn.rollback();
+			}
+		} finally {
+			if (session != null) {
+				session.close();
+			}
+		}			
 	}
 
 	public Group getGroup(int id) {
 		Session session = factory.openSession();
-		session.beginTransaction();
-		Group group = session.get(Group.class, id);
-		session.getTransaction().commit();
-		session.close();
-		return group;
-	}
-
-	public Group getGroup(String name) {
-		Session session = factory.openSession();
-		session.beginTransaction();
-		String queryString = "from classes where name = '" + name + "'";
-		Group group;
+		Transaction txn = session.getTransaction();
+		Group group = null;
 		try {
-			group = (Group) session.createQuery(queryString).getSingleResult();
+			txn.begin();
+			group = session.get(Group.class, id);
+			txn.commit();
 		} catch (Exception e) {
-			// TODO: handle exception GroupsDAO
-			return null;
-		}
-		session.getTransaction().commit();
-		session.close();
-		return group;
+			if (txn != null) {
+				e.printStackTrace();
+				txn.rollback();
+			}
+		} finally {
+			if (session != null) {
+				session.close();
+			}
+		}	
+		return group;			
 	}
 
-	public boolean isExisted(String name) {
+	public Group getGroup(String name) {		
 		Session session = factory.openSession();
-		session.beginTransaction();
+		Transaction txn = session.getTransaction();
 		String queryString = "from classes where name = '" + name + "'";
-		@SuppressWarnings("unchecked")
-		List<Group> groups = session.createQuery(queryString).getResultList();
-		session.getTransaction().commit();
-		session.close();
-		if (groups.isEmpty()) {
-			return false;
-		} else {
-			return true;
-		}
+		Group group = null;
+		try {
+			txn.begin();
+			group = (Group) session.createQuery(queryString).getSingleResult();
+			txn.commit();
+		} catch (Exception e) {
+			if (txn != null) {
+				e.printStackTrace();
+				txn.rollback();
+			}
+		} finally {
+			if (session != null) {
+				session.close();
+			}
+		}	
+		return group;			
+	}
 
+	public boolean isExisted(String name) {			
+		Session session = factory.openSession();
+		Transaction txn = session.getTransaction();
+		String queryString = "from classes where name = '" + name + "'";
+		Group group = null;
+		try {
+			txn.begin();
+			group = (Group) session.createQuery(queryString).getSingleResult();
+			txn.commit();
+		} catch (Exception e) {
+			if (txn != null) {
+				e.printStackTrace();
+				txn.rollback();
+			}
+		} finally {
+			if (session != null) {
+				session.close();
+			}
+		}	
+		return group != null;		
 	}
 
 	public void deleteGroup(int id) {
